d4c25f58d67849911d4b17b346ad86954ac3c0e0
naoki1510 <naoshio.2002@gmail.com>
2019-09-13 00:36:25 +0900

    インデックスページのカードデザインを作成

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+    mode: "development",
+    entry: ["./scss/style.scss", "./js/script.js"],
+    output: {
+        filename: "../javascript.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "../style.css"
+                        }
+                    },
+                    { loader: "extract-loader" },
+                    { loader: "css-loader" },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            plugins: () => [autoprefixer()]
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sassOptions: {
+                                includePaths: ["./node_modules"]
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["@babel/preset-env"]
+                }
+            }
+        ]
+    }
+};
