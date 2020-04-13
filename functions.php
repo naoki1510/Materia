1a237a80b0dbc9ad4ddcccd61d77e89724a0a514
naoki1510 <naoki1510@shallweband.net>
2020-04-13 15:33:26 +0900

    色カスタマイズ可

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php //スキンから親テーマの定義済み関数等をオーバーライドして設定の書き換えが可能
-if ( !defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 /*
 ///////////////////////////////////////////
 // 設定操作サンプル
@@ -11,22 +11,45 @@ if ( !defined( 'ABSPATH' ) ) exit;
 //ヘッダーロゴを「トップメニューにするコードサンプル
 if ( !function_exists( 'get_header_layout_type' ) ):
 function get_header_layout_type(){
-  return 'top_menu';
+    return 'top_menu';
 }
 endif;
 
 //メインカラム幅を680pxにするサンプル
 if ( !function_exists( 'get_main_column_contents_width' ) ):
 function get_main_column_contents_width(){
-  return 680;
+    return 680;
 }
 endif;
 
 //エントリーカードの枠線を表示するサンプル
 if ( !function_exists( 'is_entry_card_border_visible' ) ):
 function is_entry_card_border_visible(){
-  return true;
+    return true;
 }
 endif;
 
 */
+function materia_set_color_values()
+{
+
+    $properties = [
+        'site_key_color',
+        'site_key_text_color',
+        'site_background_color',
+    ];
+
+    $css = ':root {';
+
+    foreach ($properties as $property) {
+        $value = get_theme_option($property);
+        if(empty($value)) continue;
+        $css .= '--' . str_replace('_', '-', $property) . ': ' . $value . ';';
+    }
+
+    $css .= '}';
+
+    wp_add_inline_style(THEME_NAME . '-skin-style', $css);
+}
+
+add_action('wp_enqueue_scripts', 'materia_set_color_values');
